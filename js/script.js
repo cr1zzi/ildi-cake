d1afa5fa6628b30ba018ca49f55986f2dd90d203
Cristi <cr1zzi112@gmail.com>
2021-04-02 16:25:24 +0300

    almost done
    making some changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 gsap.registerPlugin(ScrollTrigger);
 
-// animations
+// loading screen
+let beeWrapper = document.querySelector(".bee-wrapper");
+window.addEventListener("load", function () {
+  beeWrapper.style.display = "none";
+});
+
+// navbar animations
 
 gsap.from(".navbar", { duration: 1, y: "-100%", ease: "bounce" });
 gsap.from(".nav-link", { duration: 2, opacity: 0, delay: 1, stagger: 0.3 });
@@ -12,15 +18,49 @@ gsap.from(".navbar-brand", {
 });
 gsap.from(".main-info", { duration: 1.5, x: "-200%", ease: "back", delay: 2 });
 
+//main page animations
+
 let tl = gsap.timeline({
   scrollTrigger: {
     trigger: ".test-photos",
-    start: "80% bottom",
+    start: "50% bottom",
+  },
+});
+
+let tlAbout = gsap.timeline({
+  scrollTrigger: {
+    trigger: "#about-id",
+    start: "150% bottom",
   },
 });
 
 tl.from(".images", { x: "300%", opacity: 0, duration: 1, stagger: 0.2 });
-tl.from(".text-test", { opacity: 0, duration: 1, stagger: 0.2 }, "-=1");
+tl.from(".text-test", { opacity: 0, duration: 0.2, stagger: 0.1 }, "-=0.3");
+
+tl.from(
+  ".about-info-text",
+  {
+    x: "-100%",
+    opacity: 0,
+    duration: 1,
+    ease: "bounce",
+    delay: 0.5,
+  },
+  "-=1"
+);
+
+tl.from(
+  ".woman-image",
+  {
+    x: "100%",
+    opacity: 0,
+    duration: 1,
+    ease: "bounce",
+    stagger: 1,
+    delay: 0.5,
+  },
+  "-=1"
+);
 
 // code for modal
 
